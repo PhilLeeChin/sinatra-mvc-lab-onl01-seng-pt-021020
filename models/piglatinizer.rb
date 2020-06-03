289a903e524f53c508e8f358d559ca968b25fe70
Philip Lee-Chin <phil.leechin@outlook.com>
2020-06-03 17:27:09 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,8 +1,8 @@
 class PigLatinizer
   attr_reader :piglatinize
 
-  def initialize(piglatinize)
-    @piglatinize = piglatinize
-  end
+  # def initialize(piglatinize)
+  #   @piglatinize = piglatinize
+  # end
 
 end
